9e5faa0b32eca2e79f9993c0f0df64be8d9ba853
scriptkidakash81 <scriptkidakash81@gmail.com>
2025-04-30 12:56:38 +0530

    Update testfile7.go

--- test/testfile7.go
+++ test/testfile7.go
@@ -1,19 +1,25 @@
 package main
 
 import (
+	"crypto/md5"
+	"database/sql"
 	"fmt"
 	"log"
+	"net/http"
+	"os/exec"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
 	// Vulnerability: Hardcoded credentials - AWS API Key
-	awsAPIKey : "AKIAIOSFODNN7EXAMPLE" // This is a hardcoded secret
+	awsAPIKey := "AKIAIOSFODNN7EXAMPLE" // This is a hardcoded secret
 
 	// Vulnerability: Hardcoded password
-	password : "SuperSecretPassword123" // Hardcoded password
+	password := "SuperSecretPassword123" // Hardcoded password
 
 	// Vulnerability: Hardcoded API key for Stripe
-	stripeAPIKey : "sk_live_4eC39HqLyjWDarjtT1zdp7dc" // Stripe API key
+	stripeAPIKey := "sk_live_4eC39HqLyjWDarjtT1zdp7dc" // Stripe API key
 
 	// Log the values (insecure to print secrets)
 	log.Println("AWS API Key:", awsAPIKey)
@@ -23,6 +29,47 @@ func main() {
 	// Performing some tasks with the credentials
 	processAWSRequest(awsAPIKey)
 	processStripePayment(stripeAPIKey)
+
+	// Vulnerability: SQL injection
+	db, err := sql.Open("mysql", "user:password@/dbname")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	userInput := "Robert'); DROP TABLE users; --"
+	query := fmt.Sprintf("SELECT * FROM users WHERE name = '%s'", userInput)
+	_, err = db.Exec(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Vulnerability: Command injection
+	cmd := exec.Command("sh", "-c", "echo "+userInput)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(output))
+
+	// Vulnerability: Weak password hashing
+	hash := md5.Sum([]byte(password))
+	fmt.Println("Password hash:", hash)
+
+	// Vulnerability: Insecure HTTP client
+	resp, err := http.Get("http://example.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	// Vulnerability: Missing HTTPS verification
+	httpClient := &http.Client{}
+	httpClient.Transport = &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+	}
 }
 
 func processAWSRequest(apiKey string) {
@@ -34,7 +81,3 @@ func processStripePayment(apiKey string) {
 	// Simulate Stripe payment processing
 	fmt.Println("Processing payment with Stripe API key:", apiKey)
 }
-#test
-#test3
-##
-##
